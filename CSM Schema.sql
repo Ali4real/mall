5bdd1ad63043c94f175a196350e0eba7b4449871
ali4real <alirasheed7122@gmail.com>
2022-06-11 01:23:01 -0700

    added table

---
+++ CSM Schema.sql
@@ -0,0 +1,25 @@
+create database Mall
+
+
+create table product(
+pid int not null,
+pname varchar(45),
+price int,
+Vname varchar(45) primary key,
+
+)
+
+create table vendor(
+vid int,
+vname varchar(45),
+adr varchar(45),
+
+)
+
+
+create table employee(
+Eid int,
+Ename varchar(45),
+Edesig varchar(45),
+
+)
