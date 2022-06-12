35a007d4b3d7b26a9c1c978959c6c04a55919dc9
iabahmad <abdullahrashad3@gmail.com>
2022-06-12 14:07:54 +0500

    module to update record is added

--- mall/WindowsFormsApp1/empup.Designer.cs
+++ mall/WindowsFormsApp1/empup.Designer.cs
@@ -130,6 +130,7 @@
             this.button1.TabIndex = 10;
             this.button1.Text = "update";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- mall/WindowsFormsApp1/empup.cs
+++ mall/WindowsFormsApp1/empup.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp1
 {
     public partial class empup : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-75IR95H\\SQLEXPRESS;Initial Catalog=mall;Integrated Security=True");
         public empup()
         {
             InitializeComponent();
@@ -22,6 +24,15 @@ namespace WindowsFormsApp1
             Form1 f1 = new Form1();
             this.Hide();
             f1.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update product set pid ='"+textBox2.Text+"', pname = '"+textBox3.Text+"', price = '"+textBox4.Text+"', Vname='"+textBox5.Text+"' where pid ='"+textBox1.Text+"'",con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("updated successfully");
         }
     }
 }
