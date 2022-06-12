fdea05d544daed5b7b11b73f5256bfa5f2e6a878
iabahmad <abdullahrashad3@gmail.com>
2022-06-12 17:12:17 +0500

    upgraded admin console

--- mall/WindowsFormsApp1/adminlogin.cs
+++ mall/WindowsFormsApp1/adminlogin.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp1
 {
     public partial class adminlogin : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-75IR95H\\SQLEXPRESS;Initial Catalog=mall;Integrated Security=True");
         public adminlogin()
         {
             InitializeComponent();
@@ -26,6 +28,20 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select count(*) from adm", con);
+            int a = (int)cmd.ExecuteScalar();
+            if (a > 0)
+            {
+                adminterface af = new adminterface();
+                af.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("invalid ceredendials....!");
+                con.Close();
+            }
 
         }
     }
